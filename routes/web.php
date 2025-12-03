65c6fb992edf8a2c78d1a53762359aabceed22fc
augusto-dmh <augustodemelohenriques@gmail.com>
2025-12-02 22:03:06 -0300

    feat: add EmpresaController with crud actions + routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\EmpresaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('empresa')->name('empresa.')->group(function () {
+    Route::get('/', [EmpresaController::class, 'index'])->name('index');
+    Route::get('/cadastro', [EmpresaController::class, 'create'])->name('create');
+    Route::post('/', [EmpresaController::class, 'store'])->name('store');
+    Route::get('/{id}/relatorio', [EmpresaController::class, 'relatorio'])->name('relatorio');
 });
